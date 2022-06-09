04e29bbe7835c1faa0ea667737f9d4ab4ed01d9e
Dmitry Kaveshnikov <kawwa@me.com>
2022-06-09 19:51:22 +0300

    Third Done

--- thirdAlgo/solution.swift
+++ thirdAlgo/solution.swift
@@ -1,1 +1,36 @@
 import Foundation
+
+let fileName = "input0.txt"
+
+func readNums() {
+    guard let line = try? String(contentsOfFile: fileName) else {
+        return
+    }
+    let split = line.split(separator: "\n")
+
+
+   let nums = split[1].split(separator: " ").map { Int ($0)! }
+    let result = String(maxTrade(nums))
+
+    try? result.write(toFile: "output.txt", atomically: true, encoding: .utf8)
+}
+
+func maxTrade(_ prices: [Int]) -> Int {
+    var maxProfit = 0
+    var minPrice = prices[0]
+    
+    if prices.isEmpty {
+        return maxProfit
+    }
+    
+    for i in 0..<prices.count {
+        if prices[i]<minPrice {
+            minPrice = prices[i]
+        } else {
+            maxProfit += prices[i] - minPrice
+            minPrice = prices[i]
+        }
+    }
+    return maxProfit
+}
+readNums()
